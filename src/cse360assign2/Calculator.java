364c8236ba4e2f1f4c5ba78164bc314d0c6f62e2
SaadiqWebster <saadiqwebster@gmail.com>
2019-02-24 22:25:26 -0700

    added comments

--- src/cse360assign2/Calculator.java
+++ src/cse360assign2/Calculator.java
@@ -1,5 +1,65 @@
+/**
+ * Saadiq Webster
+ * This class emulates adding, subtracting, multiplying,
+ * and dividing from a real life calculator
+ */
 package cse360assign2;
 
 public class Calculator {
 
+	private int total;
+	
+	/**
+	 * Class Constructor
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * returns the current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * adds the value to the total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtracts the value to the total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiplies the value to the total
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divides the value to the total
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * summarizes all the method calls
+	 * @return a strng
+	 */
+	public String getHistory () {
+		return "";
+	}
 }
